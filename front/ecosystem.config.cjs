845edb4702e538638a1746919d47a8ca58e03098
Mursch Arthur <149969486+Crrux@users.noreply.github.com>
2025-06-17 18:17:20 +0200

    save

--- front/ecosystem.config.cjs
+++ front/ecosystem.config.cjs
@@ -8,6 +8,8 @@ module.exports = {
             NODE_ENV: 'production',
         },
         error_file: './logs/err.log',
-        out_file: './logs/out.log'
+        out_file: './logs/out.log',
+        merge_logs: true,
+        autorestart: true
     }]
 }
